5b3b40e2aeac9622164a1a4f7fcb312522faac37
gabriel <gbordesc@uft.edu>
2022-05-12 13:57:54 -0400

    avances

--- lib/pages/login/login_page.dart
+++ lib/pages/login/login_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:email_validator/email_validator.dart';
 
 class LoginPage extends StatelessWidget {
   @override
@@ -25,6 +26,41 @@ class __LoginScreenState extends State<_LoginScreen> {
   final TextEditingController _controllerPassword = TextEditingController();
 
   final _formKey = GlobalKey<FormState>();
+  final _scaffoldKey = GlobalKey<ScaffoldState>();
+  late String _email;
+  late String _password;
+
+  void _submitCommand() {
+    //get state of our Form
+    final form = _formKey.currentState;
+
+    //`validate()` validates every FormField that is a descendant of this Form,
+    // and returns true if there are no errors.
+    if (form!.validate()) {
+      //`save()` Saves every FormField that is a descendant of this Form.
+      form.save();
+
+      // Email & password matched our validation rules
+      // and are saved to _email and _password fields.
+      if (_loginCommand()) {
+        // Navigate to our Home page
+        Navigator.pushReplacementNamed(context, '/home');
+      } else {
+        // Show a snackbar with a message
+      }
+    }
+  }
+
+  bool _loginCommand() {
+    // Show login details in snackbar
+    final snackbar = SnackBar(
+      content: Text('Email: $_email, password: $_password'),
+    );
+    // _scaffoldKey.currentState!.showSnackBar(snackbar);
+    _scaffoldKey.currentState!.showSnackBar(snackbar);
+
+    return true;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -53,6 +89,10 @@ class __LoginScreenState extends State<_LoginScreen> {
                 labelText: 'Email',
                 prefixIcon: Icon(Icons.email),
               ),
+              validator: (val) => !EmailValidator.validate(val!, true)
+                  ? 'Ingresa un mail valido'
+                  : null,
+              onSaved: (val) => _email = val!,
             ),
             SizedBox(
               height: 20,
@@ -64,6 +104,13 @@ class __LoginScreenState extends State<_LoginScreen> {
                 labelText: 'Password',
                 prefixIcon: Icon(Icons.lock),
               ),
+              validator: (String? value) {
+                if (value!.isEmpty) {
+                  return 'Por favor agrega una pass';
+                }
+                return null;
+              },
+              onSaved: (val) => _password = val!,
             ),
             SizedBox(
               height: 20,
@@ -71,7 +118,7 @@ class __LoginScreenState extends State<_LoginScreen> {
             Center(
                 child: ElevatedButton(
               child: Text('Login'),
-              onPressed: () => Navigator.pushNamed(context, '/'),
+              onPressed: () => _submitCommand(),
             )),
             SizedBox(
               height: 10,
@@ -86,6 +133,7 @@ class __LoginScreenState extends State<_LoginScreen> {
                 child: ElevatedButton(
               child: Text('Registro'),
               onPressed: () => Navigator.pushNamed(context, '/register'),
+              // onPressed: _submitCommand,
             )),
           ]),
     );
